2a49c24f0c1d73ff16e994514fa82ac94fe3b226
Agata-Liberty7 <119856111+Agata-Liberty7@users.noreply.github.com>
2024-11-11 17:32:28 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,6 @@ from dotenv import load_dotenv
 load_dotenv()
 updater = Updater(os.getenv("TELEGRAM_API_TOKEN"), use_context=True)
 
-
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(MessageHandler(Filters.text, handle_message))
